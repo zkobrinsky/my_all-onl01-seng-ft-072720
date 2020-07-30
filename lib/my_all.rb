afeab086c921bc853ebb08c672dd555a4565eb91
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 19:19:59 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,9 +4,10 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
+    binding.pry
     block_return_values << yield(collection[i])
     i = i+1
-    binding.pry
+    
   end
     block_return_values
 end
