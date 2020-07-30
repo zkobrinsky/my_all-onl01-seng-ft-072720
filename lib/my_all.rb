c06892138a66f2af30b14a84bc60f06dad4998cb
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 19:25:37 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,10 +8,12 @@ def my_all?(collection)
     i = i+1
   end
   if block_return_values.include?(false)
+    puts "false"
     false
   else
+    puts "true"
     true
 end
 
-my_all?([1, 1, 2, 3, 4, 5]) {|i| i<3 puts "#{i}"}
+my_all?([1, 1, 2, 3, 4, 5]) {|i| i<3}
 # binding.pry
